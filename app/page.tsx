f41549669c9d80fcb358567ef80aee8085edca0f
TranTienThe <tranthe2k2ak@gmail.com>
2024-08-31 23:21:09 +0700

    Complete setup and configuration for Haibazo game

--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,155 @@
-import Image from "next/image";
+'use client';
+import { useState, useEffect, useRef } from 'react';
+import Swal from 'sweetalert2';
 
 export default function Home() {
+  const [points, setPoints] = useState(0);
+  const [numbers, setNumbers] = useState([]);
+  const [positions, setPositions] = useState([]);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [time, setTime] = useState(0);
+  const [nextNumber, setNextNumber] = useState(1);
+  const numberParentRef = useRef(null);
+
+  const handleInputChange = (e: any) => {
+    setPoints(parseInt(e.target.value));
+  };
+
+  const handlePlayClick = () => {
+    setIsPlaying(false);
+    setNumbers([]);
+    setPositions([]);
+
+    setTime(0);
+    setNextNumber(1);
+
+    const numberList = [];
+    for (let i = 1; i <= points; i++) {
+      numberList.push(i);
+    }
+    const shuffledNumbers = shuffleArray(numberList);
+    setNumbers(shuffledNumbers);
+
+    const generatedPositions = shuffledNumbers.map(() => ({
+      top: `${Math.random() * 91}%`,
+      left: `${Math.random() * 91}%`,
+    }));
+    setPositions(generatedPositions);
+
+    resetNumberDisplay();
+    setIsPlaying(true);
+  };
+
+  const resetNumberDisplay = () => {
+    if (numberParentRef.current) {
+      const spans = numberParentRef.current.querySelectorAll('span');
+
+      spans.forEach((span: any) => {
+        span.style.display = 'block';
+      });
+    }
+  };
+
+  const shuffleArray = (array: any) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  };
+
+  useEffect(() => {
+    let interval: any;
+
+    if (isPlaying) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 1);
+      }, 1000);
+    } else {
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval);
+  }, [isPlaying]);
+
+  const handleNumberClick = (e: any, number: number) => {
+    if (number < nextNumber) return;
+    if (number > nextNumber) {
+      Swal.fire({
+        icon: 'error',
+        title: 'You lose!',
+        text: 'Press play to try again.',
+      });
+      setIsPlaying(false);
+      return;
+    }
+
+    e.target.style.transition = 'all 1s ease-in-out, transform 1s ease-in-out';
+    e.target.style.opacity = '0';
+    e.target.style.transform = 'scale(0.5)';
+    setTimeout(() => {
+      e.target.style.display = 'none';
+      e.target.style.opacity = '1';
+      e.target.style.transform = 'scale(1)';
+      e.target.style.transition = '';
+    }, 1000);
+
+    if (nextNumber === points) {
+      Swal.fire({
+        icon: 'success',
+        title: 'Congratulations!',
+        text: 'You win!',
+      });
+      setIsPlaying(false);
+      return;
+    }
+
+    setNextNumber(nextNumber + 1);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <main className="flex min-h-screen flex-col items-center justify-between p-24n bg-white">
+      <div className="w-[95%] mt-4 rounded-sm border-[3px] border-black h-full">
+        <h2 className="text-center mt-5 text-[24px] font-bold">HABAZO - Entrance test - Trần Tiến Thế - React JS</h2>
+        <div className="flex justify-between ml-[250px] mr-[250px]">
+          <div className="flex">
+            <h2 className="mt-5 text-[18px] font-bold">Points :</h2>
+            <input
+              type="number"
+              className="border-[2px] border-black ml-5 mt-5 font-bold w-[100px] text-center"
+              value={points === 0 ? '' : points}
+              onChange={handleInputChange}
+              // disabled={isPlaying}
             />
-          </a>
+          </div>
+          <div className="flex">
+            <h2 className="mt-5 text-[18px] font-bold">Time :</h2>
+            <h2 className="mt-5 text-[18px] font-bold ml-5">{time}s</h2> {/* Display timer */}
+          </div>
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
+        <div className="flex justify-center">
+          <button className="mt-3 text-[18px] font-bold border-[2px] border-red-500 rounded-full w-[120px] h-[50px] hover:bg-red-500 hover:text-white" onClick={handlePlayClick}>
+            {isPlaying ? 'Reset' : 'Play'}
+          </button>
+        </div>
+        <div ref={numberParentRef} className="w-[98%] ml-4 mt-4 rounded-sm border-[3px] border-black h-[500px] mb-5 relative">
+          {numbers.map((number, index) => (
+            <span
+              key={number}
+              className={`absolute text-[24px] font-bold text-red-400 border-2 border-red-400 cursor-pointer bg-white hover:bg-red-700 hover:text-white transition-transform duration-150 ease-in-out`}
+              onClick={(e) => handleNumberClick(e, number)}
+              style={{
+                top: positions[index]?.top,
+                left: positions[index]?.left,
+                borderRadius: '50px',
+                padding: '5px 20px',
+                zIndex: `${points - number}`,
+              }}
+            >
+              {number}
             </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          ))}
+        </div>
       </div>
     </main>
   );
